c4c248d323cd941077bc4e7fb33c19d77e53a0fe
David Mendoza <davidmendozaofficial@gmail.com>
2022-07-01 08:46:48 +0800

    auth part1
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('login', function(){
-    return view('auth/login');
+    // return view('auth/login');
 })->name('login');
 
 Route::group([
