ccc7f611ef3f664a2e82931c7657bd15c795afe3
David Mendoza <davidmendozaofficial@gmail.com>
2023-07-18 14:41:28 +0800

    defaults
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>{{ "System Name | " . "Login" }}</title>
+    <title>{{ env("APP_NAME") . " | " . "Login" }}</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="index3.html" class="brand-link">
-        <img src="{{ asset('images/pharmacy_logo.png') }}" alt="Pharmacy Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Malolos Pharmacy</span>
+        <img src="{{ asset('images/logo.png') }}" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">{{ env("APP_NAME") }}</span>
     </a>
 
     <div class="sidebar">
